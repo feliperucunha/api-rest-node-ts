66a77df8362bcf3e1d287e5a0d1629445a1f386a
Felipe Cunha <feliperubenmv@gmail.com>
2020-08-22 11:36:30 -0300

    Controller functionality added (abstraction)

--- src/routes.ts
+++ src/routes.ts
@@ -1,69 +1,10 @@
 import express, { response } from 'express';
-import db from './database/connection'
-import converHourToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
 
-interface ScheduleItem {
-    week_day: number;
-    from: string;
-    to: string;
-}
 
-routes.post("/classes", async (req, res) => {
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = req.body;
-
-    const trx = await db.transaction();
-
-    try {
-        const insertedUsersIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio,
-        });
-    
-        const user_id = insertedUsersIds[0];
-    
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            whatsapp,
-            bio,
-            user_id,
-        });
-    
-        const class_id = insertedClassesIds[0];
-    
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: converHourToMinutes(scheduleItem.from),
-                to: converHourToMinutes(scheduleItem.to),
-            };
-        })
-    
-        await trx('class_schedule').insert(classSchedule);
-    
-        await trx.commit();
-    
-        return res.status(201).send();
-    } catch (error) {
-        console.log(error)
-        await trx.rollback();
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
-    }
-})
+routes.post("/classes", classesControllers.create);
 
 export default routes;
